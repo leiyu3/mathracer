00181e6e8bb2d978c4ef882ded9fe225d0da9bbc
leiyu3 <s444814187@gmail.com>
2022-03-17 12:33:48 -0400

    can now check answers!

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ let time = document.getElementById("time");
 let question = document.getElementById("question");
 let answer = document.getElementById("answer");
 let feedback = document.getElementById("feedback");
+let xy = genQuestion();
 
 answer.addEventListener("keydown", function (e) {
     if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
-        checkAnswer(e);
+        checkAnswer();
     }
 });
 
 
-function checkAnswer(ans)
+function checkAnswer()
 {
-    answer = ans.target.value;
-    if (answer == 8)
+    if (answer.value == xy[0] * xy[1])
     {
         feedback.innerHTML = "Correct!";
     }
@@ -22,5 +22,17 @@ function checkAnswer(ans)
     {
         feedback.innerHTML = "Incorrect!";
     }
-    ans.target.value = NaN;
+    answer.value = NaN;
+    xy = genQuestion();
 }
+
+function genQuestion()
+{
+    // Generate 2 number from 0 to 9;
+    let x = Math.floor(Math.random() * 9) + 1;
+    let y = Math.floor(Math.random() * 9) + 1;
+
+    question.innerHTML = x + "*" + y;
+    return [x,y];
+}
+
