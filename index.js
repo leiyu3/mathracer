6a654ff289a20a4b1652efd4ec2c55f6280f886b
leiyu3 <s444814187@gmail.com>
2022-03-18 18:23:47 -0400

    fix the timer not adding to minutes at 60s

--- index.js
+++ index.js
@@ -75,5 +75,9 @@ function genQuestion()
 function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    return (
+        seconds == 60 ?
+        (minutes+1) + ":00" :
+        minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+      );
   }
