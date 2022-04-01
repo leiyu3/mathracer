cf394409e8b94b299ae9207a1549bcc810bd2ba2
leiyu3 <s444814187@gmail.com>
2022-03-31 23:16:37 -0400

    changes default to standard mode
    mvp without checking wrong inputs from serverside

--- index.js
+++ index.js
@@ -1,56 +1,38 @@
+// select elements from html
 let score = document.getElementById("score");
 let timer = document.getElementById("time");
 let question = document.getElementById("question");
 let answer = document.getElementById("answer");
 let feedback = document.getElementById("feedback");
-let xy = genQuestion();
+
+let mode = 0;
 let point = 0;
-let question_quantity = '∞';
+
 let lastTime, ms;
 ms = 0;
 timer.textContent = "0:00";
-
-
 let numberList = [1,2,3,4,5,6,7,8,9];
 
-function permutate(numbers)
-{
-    let questions = [];
+let questionList = shuffle(permutate(numberList));
+let xy = genQuestion();
 
-    for (let i = 1; i < numbers.length + 1; i++)
-    {
-        for (let j = i; j < numbers.length + 1; j++)
-        {
-            questions.push([i,j]);
-        }
-    }
-    return questions;
+
+let question_quantity = '';
+if (mode == 0)
+{
+    question_quantity = questionList.length + 1;
+}
+else if (mode == 1)
+{
+    question_quantity = '∞';
 }
 
-function shuffle(list)
-{
-    let newList = [];
-    let n = list.length;
-    while (n)
-    {
-        i = Math.floor(Math.random() * list.length);
-        if (i in list)
-        {
-            newList.push(list[i]);
-            delete list[i];
-            n--;
-        }
-    }
-    return newList;
-}
-
-questionList = shuffle(permutate(numberList));
-
-score.textContent = point + '/' + questionList.length;
+score.textContent = point + '/' + question_quantity;
 
 
 function update(time)
 {
+    // Updates the game state
     if (lastTime != null)
     {
         const delta = time - lastTime;
@@ -58,14 +40,16 @@ function update(time)
         timer.textContent = millisToMinutesAndSeconds(ms);
     }
 
-    lastTime = time;
-    window.requestAnimationFrame(update);
+    if (mode != 99)
+    {
+        lastTime = time;
+        window.requestAnimationFrame(update);
+    }
 }
 
-
-
 answer.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+    //checks whether the pressed key is "Enter"
+    if (e.code === "Enter") {  
         checkAnswer();
         if (lastTime == null)
         {
@@ -73,7 +57,6 @@ answer.addEventListener("keydown", function (e) {
         }
     }
 });
-
 
 function checkAnswer()
 {
@@ -88,17 +71,36 @@ function checkAnswer()
     }
     score.textContent = point + '/' + question_quantity;
     answer.value = NaN;
+    if (questionList.length == 0)
+    {
+        feedback.textContent = "Completed in " + millisToMinutesAndSeconds(ms) + "!";
+        mode = 99;
+        answer.disabled = true;
+        return;
+    }
     xy = genQuestion();
 }
 
 function genQuestion()
 {
     // Generate 2 number from 0 to 9;
-    let x = Math.floor(Math.random() * 9) + 1;
-    let y = Math.floor(Math.random() * 9) + 1;
+    let x = 0;
+    let y = 0;
+    if (mode == 0)
+    {
+        x = questionList[0][0];
+        y = questionList[0][1];
+        questionList.shift();
+    }
+    else if (mode == 1)
+    {
+        x = Math.floor(Math.random() * 9) + 1;
+        y = Math.floor(Math.random() * 9) + 1;
+    }
 
-    question.textContent = x + "*" + y;
+    question.textContent = x + "×" + y;
     return [x,y];
+
 }
 
 function millisToMinutesAndSeconds(millis) {
@@ -110,3 +112,36 @@ function millisToMinutesAndSeconds(millis) {
         minutes + ":" + (seconds < 10 ? "0" : "") + seconds
       );
   }
+
+function permutate(numbers)
+{
+    // Generate permutations of a list
+    let questions = [];
+
+    for (let i = 1; i < numbers.length + 1; i++)
+    {
+        for (let j = i; j < numbers.length + 1; j++)
+        {
+            questions.push([i,j]);
+        }
+    }
+    return questions;
+}
+
+function shuffle(list)
+{
+    // Shuffles a list
+    let newList = [];
+    let n = list.length;
+    while (n)
+    {
+        i = Math.floor(Math.random() * list.length);
+        if (i in list)
+        {
+            newList.push(list[i]);
+            delete list[i];
+            n--;
+        }
+    }
+    return newList;
+}
